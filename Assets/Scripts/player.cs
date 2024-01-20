1af1d9246398609d35fede599ff8ae6ae0bf5fa7
Jackson James <jackson.james@converj.com>
2024-01-20 19:35:59 +0530

    crouch collider

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Animator animator;
+    private float crouchOffset = 0.5f;
+    private float crouchSize = 0.8f;
 
+
+    private BoxCollider2D playerCollider;
+    private Vector2 originalSize;
+
+
+
+    void Start()
+    {
+        playerCollider = GetComponent<BoxCollider2D>();
+        originalSize = playerCollider.offset;
+    }
 
     void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
-        Debug.Log("speed"+ speed);
+        float vertical = Input.GetAxisRaw("Vertical");
         Vector3 scale=transform.localScale;
         animator.SetFloat("speed", Mathf.Abs(speed));
 
@@ -20,6 +33,26 @@ public class Player : MonoBehaviour
         }
         else if (speed>0){
             scale.x=Mathf.Abs(scale.x);
+        }
+
+        if(vertical<0){
+            animator.SetBool("crouch", true);
+            playerCollider.offset= new Vector2(playerCollider.offset.x, crouchOffset);
+            playerCollider.size = new Vector2(playerCollider.size.x, crouchSize);
+
+        }
+        else if (vertical > 0)
+        {
+            animator.SetBool("jump", true);
+
+        }
+        else
+        {
+            animator.SetBool("crouch", false);
+            animator.SetBool("jump", false);
+            playerCollider.offset = new Vector2(playerCollider.offset.x, playerCollider.offset.y);
+            playerCollider.size = new Vector2(playerCollider.size.x, playerCollider.size.y);
+
         }
         transform.localScale = scale;
     }
