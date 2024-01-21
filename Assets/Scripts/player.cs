83864ee355495f584d0a8e2b1f9f30341886ea45
Jackson James <jackson.james@converj.com>
2024-01-21 10:58:42 +0530

    movement

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -7,12 +7,18 @@ public class Player : MonoBehaviour
     public Animator animator;
     private float crouchOffset = 0.5f;
     private float crouchSize = 0.8f;
-
-
     private BoxCollider2D playerCollider;
     private Vector2 originalSize;
+    public float speed;
+    private Rigidbody2D rb2d;
+    public float jump;
 
 
+
+    void Awake()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
 
     void Start()
     {
@@ -22,22 +28,30 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
+        float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 scale=transform.localScale;
-        animator.SetFloat("speed", Mathf.Abs(speed));
+        PlayerMovementAnimation(horizontal,vertical);
+        MoveCharacter(horizontal,vertical);
+    }
 
-        if (speed < 0)
+    private void PlayerMovementAnimation(float horizontal,float vertical)
+    {
+        Vector3 scale = transform.localScale;
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed>0){
-            scale.x=Mathf.Abs(scale.x);
+        else if (horizontal > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
         }
 
-        if(vertical<0){
+        if (vertical < 0)
+        {
             animator.SetBool("crouch", true);
-            playerCollider.offset= new Vector2(playerCollider.offset.x, crouchOffset);
+            playerCollider.offset = new Vector2(playerCollider.offset.x, crouchOffset);
             playerCollider.size = new Vector2(playerCollider.size.x, crouchSize);
 
         }
@@ -55,5 +69,24 @@ public class Player : MonoBehaviour
 
         }
         transform.localScale = scale;
+
+    }
+    private void MoveCharacter(float horizontal,float vertical)
+    {
+        Vector3 position = transform.position;
+        position.x = position.x + horizontal * speed * Time.deltaTime;
+        transform.position=position;
+
+        if(vertical> 0)
+        {
+            rb2d.AddForce(new Vector2 (0f, jump),ForceMode2D.Force);
+        }
+
+
+
+        
+
+
+
     }
 }
